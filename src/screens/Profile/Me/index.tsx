879a6e6b117773ff648fafe54d4a67d6d1996320
Gabriel-Lucena <gabriel.lucena.cunha@hotmail.com>
2022-04-29 10:04:32 -0300

    Inserindo ícone de câmera

--- src/screens/Profile/Me/index.tsx
+++ src/screens/Profile/Me/index.tsx
@@ -1,33 +1,73 @@
-import { useEffect, useState } from 'react';
-import { Container } from './styles';
+import React, { useEffect, useState, useRef } from 'react';
+import { KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import {
-  KeyboardAvoidingView,
-  TouchableWithoutFeedback,
-  Keyboard,
-  Text,
-  View
-} from 'react-native';
+  ButtonSave,
+  Container,
+  ButtonSaveText,
+  ButtonView,
+  Bold,
+  NotBold,
+  ImageView,
+  ProfilePicture,
+  NotificationsContainer,
+  NotificationIndicator,
+  ProfilePictureContainer,
+  CameraIcon,
+} from './styles';
 
-import { useNavigation } from '@react-navigation/native';
-import { ContentContainer } from '../../Login/styles';
-import { ScrollView } from 'react-native-gesture-handler';
+import BellIcon from '../../../assets/icons/bell.svg';
+import Photo from '../../../assets/icons/photo.svg';
+
+import { NameInput } from '../../../components/Profile/NameInput';
+import { UserInput } from '../../../components/Profile/UserInput';
+import { EmailInput } from '../../../components/Profile/EmailInput';
+import { Modalize } from 'react-native-modalize';
 
 export function ProfileMe() {
   const navigation = useNavigation();
   const [state, setState] = useState();
+  const modalizeNotificationsRef = useRef<Modalize>(null);
+
+  const handleOpenNotificationsModal = () => {
+    modalizeNotificationsRef.current?.open();
+  };
 
   useEffect(() => {}, []);
 
   return (
-    <KeyboardAvoidingView behavior='position' enabled>
-      <ScrollView>
-        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-          <Container>
-            <ContentContainer></ContentContainer>
-          </Container>
-        </TouchableWithoutFeedback>
-      </ScrollView>
+    <KeyboardAvoidingView behavior="position" enabled>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <Container>
+          <ImageView>
+            <ProfilePictureContainer>
+              <ProfilePicture source={{ uri: 'https://imgur.com/random.png' }} />
+              <CameraIcon>
+                <Photo width={40} height={40} viewBox={"CameraIcon"} ></Photo>
+              </CameraIcon>
+            </ProfilePictureContainer>
+
+            <NotificationsContainer onPress={handleOpenNotificationsModal}>
+              <NotificationIndicator>3</NotificationIndicator>
+              <BellIcon width={36} height={36} />
+            </NotificationsContainer>
+          </ImageView>
+
+          <ButtonView>
+            <Bold>Dados</Bold>
+            <NotBold>Trocar senha</NotBold>
+          </ButtonView>
+
+          <NameInput />
+          <UserInput />
+          <EmailInput />
+
+          <ButtonSave>
+            <ButtonSaveText>Salvar alterações</ButtonSaveText>
+          </ButtonSave>
+        </Container>
+      </TouchableWithoutFeedback>
     </KeyboardAvoidingView>
   );
 }
--- src/screens/Profile/Me/styles.ts
+++ src/screens/Profile/Me/styles.ts
@@ -1,12 +1,107 @@
 import styled from 'styled-components/native';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
-import { RFPercentage } from 'react-native-responsive-fontsize';
+import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
 
 export const Container = styled.View`
   align-items: center;
+  background-color: ${({ theme }) => theme.colors.light.background};
   height: ${RFPercentage(100) + getStatusBarHeight()}px;
+  margin-top: ${getStatusBarHeight()}px;
 `;
 
-export const ContentContainer = styled.View`
-  
-`
+export const ProfilePictureContainer = styled.TouchableOpacity``;
+
+export const NotificationsContainer = styled.TouchableOpacity``;
+
+export const NotificationIndicator = styled.Text`
+  background-color: ${({ theme }) => theme.colors.primary};
+  color: ${({ theme }) => theme.colors.button_text};
+  width: 20px;
+  height: 20px;
+  border-radius: 12px;
+
+  text-align: center;
+  font-size: ${RFValue(13)}px;
+  font-family: ${({ theme }) => theme.fonts.primary_400};
+
+  position: absolute;
+  z-index: 10;
+  right: -3px;
+  top: -3px;
+`;
+
+export const CameraIcon = styled.Text`
+  background-color: ${({ theme }) => theme.colors.primary};
+
+  width: 22px;
+  height: 22px;
+  border-radius: 2px;
+
+  position: absolute;
+  right: -1px;
+  top: 140px;
+`;
+
+export const ImageView = styled.View`
+  display: flex;
+  flexDirection: row;
+
+  width: 90%;
+  justify-content: center;
+
+  marginLeft: ${RFPercentage(3)}px;
+  marginTop: ${RFPercentage(3)}px;
+`;
+
+export const ProfilePicture = styled.Image`
+  width: ${RFValue(200)}px;
+  height: ${RFValue(200)}px;
+
+  border-radius: ${RFValue(150)}px;
+`;
+
+export const Bold = styled.Text`
+  fontFamily: ${({ theme }) => theme.fonts.primary_500};
+  font-size: ${RFValue(20)}px;
+
+  border: none;
+  borderBottomWidth: ${RFValue(1)}px;
+  borderColor: ${({ theme }) => theme.colors.primaryDark};
+`;
+
+export const NotBold = styled.Text`
+  fontFamily: ${({ theme }) => theme.fonts.primary_400};
+  font-size: ${RFValue(20)}px;
+`;
+
+export const ButtonSave = styled.TouchableOpacity`
+  display: flex;
+  alignItems: center;
+  justify-content: center;
+
+  width: 90%;
+  height: 10%;
+
+  borderRadius: 5px;
+
+  backgroundColor: ${({ theme }) => theme.colors.primaryLight};
+`;
+
+export const ButtonSaveText = styled.Text`
+  color: ${({ theme }) => theme.colors.button_text};
+
+  font-size: ${RFValue(20)}px;
+`;
+
+export const ButtonView = styled.View`
+  display: flex;
+  flexDirection: row;
+  justify-content: space-evenly;
+
+  width: 90%;
+
+  marginTop: ${RFPercentage(2)}px;
+
+  borderBottomWidth: 0.2px;
+  border: none;
+`;
