cd0b03bfdcd25c91079d0bf187bef96c511680de
Gabriel-Lucena <gabriel.lucena.cunha@hotmail.com>
2022-04-29 08:47:32 -0300

    Bug de variável de ambiente corrigido

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,6 @@
+import React from 'react';
 import axios from "axios";
-import { SERVER_URL } from "@env";
+import { SERVER_URL } from "react-native-dotenv";
 
 console.log(SERVER_URL);
 const api = axios.create({
