667b9ca18b774ca299bf40e3c177b3cf5a6aa861
Pedro Henrique de Sousa <pedrozica.phs21@gmail.com>
2022-05-02 11:30:43 -0300

    IntroSlider finalizada

--- src/screens/IntroSlider/pagination.tsx
+++ src/screens/IntroSlider/pagination.tsx
@@ -8,19 +8,21 @@ import {
   StatusBar,
   StyleSheet,
   Text,
-  View
+  View,
+  ImageBackground
 } from 'react-native';
 
 import theme from '../../styles/theme';
+import background from '../../assets/images/gggs_background.png';
 
 export function IntroSlider() {
-  const [sliderState, setSliderState] = useState({ currentPage: 0 });
+  const [sliderState, setSliderState] = useState({ currentPage: 1 });
   const { width, height } = Dimensions.get('window');
 
   const setSliderPage = (event: any) => {
     const { currentPage } = sliderState;
     const { x } = event.nativeEvent.contentOffset;
-    const indexOfNextScreen = Math.floor(x / width);
+    const indexOfNextScreen = Math.round(x / width);
     if (indexOfNextScreen !== currentPage) {
       setSliderState({
         ...sliderState,
@@ -34,97 +36,104 @@ export function IntroSlider() {
     <>
       <StatusBar barStyle='dark-content' />
       <SafeAreaView style={{ flex: 1 }}>
-        <ScrollView
-          style={{ flex: 1 }}
-          horizontal={true}
-          scrollEventThrottle={16}
-          pagingEnabled={true}
-          showsHorizontalScrollIndicator={false}
-          onScroll={(event: any) => {
-            setSliderPage(event);
-          }}>
-          <View style={{ width, height }}>
-            <Image
-              source={require('../../assets/images/logo.svg')}
-              style={styles.imageStyle}
-            />
-            <View style={styles.wrapper}>
-              <Text style={styles.header}>
-                Est tincidunt et ac pretium id tortor molestie.
-              </Text>
-              <Text style={styles.paragraph}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tortor.
-              </Text>
+        <ImageBackground source={background} resizeMode='cover' style={styles.image}>
+          <ScrollView
+            style={{ flex: 1 }}
+            horizontal={true}
+            scrollEventThrottle={0.2}
+            pagingEnabled={true}
+            showsHorizontalScrollIndicator={false}
+            onScroll={(event: any) => {
+              setSliderPage(event);
+            }}>
+            <View style={{ width, height }}>
+              <Image
+                source={require('../../assets/images/logo.png')}
+                style={styles.imageStyle}
+              />
+              <View style={styles.wrapper}>
+                <Text style={styles.header}>
+                  Est tincidunt et ac pretium id tortor molestie.
+                </Text>
+                <Text style={styles.paragraph}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Tortor.
+                </Text>
+              </View>
             </View>
-          </View>
-          <View style={{ width, height }}>
-            <Image
-              source={require('../../assets/images/logo.svg')}
-              style={styles.imageStyle}
-            />
-            <View style={styles.wrapper}>
-              <Text style={styles.header}>
-                Nibh neque tristique orci mollis in est.
-              </Text>
-              <Text style={styles.paragraph}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tortor.
-              </Text>
+            <View style={{ width, height }}>
+              <Image
+                source={require('../../assets/images/logo.png')}
+                style={styles.imageStyle}
+              />
+              <View style={styles.wrapper}>
+                <Text style={styles.header}>
+                  Nibh neque tristique orci mollis in est.
+                </Text>
+                <Text style={styles.paragraph}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Tortor.
+                </Text>
+              </View>
             </View>
-          </View>
-          <View style={{ width, height }}>
-            <Image
-              source={require('../../assets/images/logo.svg')}
-              style={styles.imageStyle}
-            />
-            <View style={styles.wrapper}>
-              <Text style={styles.header}>
-                Elit commodo non egestas iaculis sem.
-              </Text>
-              <Text style={styles.paragraph}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tortor.
-              </Text>
+            <View style={{ width, height }}>
+              <Image
+                source={require('../../assets/images/logo.png')}
+                style={styles.imageStyle}
+              />
+              <View style={styles.wrapper}>
+                <Text style={styles.header}>
+                  Elit commodo non egestas iaculis sem.
+                </Text>
+                <Text style={styles.paragraph}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Tortor.
+                </Text>
+              </View>
             </View>
-          </View>
-          <View style={{ width, height }}>
-            <Image
-              source={require('../../assets/images/logo.svg')}
-              style={styles.imageStyle}
-            />
-            <View style={styles.wrapper}>
-              <Text style={styles.header}>
-                Nunc nibh est egestas eu ornare vel.
-              </Text>
-              <Text style={styles.paragraph}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tortor.
-              </Text>
+            <View style={{ width, height }}>
+              <Image
+                source={require('../../assets/images/logo.png')}
+                style={styles.imageStyle}
+              />
+              <View style={styles.wrapper}>
+                <Text style={styles.header}>
+                  Nunc nibh est egestas eu ornare vel.
+                </Text>
+                <Text style={styles.paragraph}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Tortor.
+                </Text>
+              </View>
             </View>
-          </View>
-          <View style={{ width, height }}>
-            <Image
-              source={require('../../assets/images/logo.svg')}
-              style={styles.imageStyle}
-            />
-            <View style={styles.wrapper}>
-              <Text style={styles.header}>
-                Posuere lorem erat at interdum adipiscing.
-              </Text>
-              <Text style={styles.paragraph}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tortor.
-              </Text>
+            <View style={{ width, height }}>
+              <Image
+                source={require('../../assets/images/logo.png')}
+                style={styles.imageStyle}
+              />
+              <View style={styles.wrapper}>
+                <Text style={styles.header}>
+                  Posuere lorem erat at interdum adipiscing.
+                </Text>
+                <Text style={styles.paragraph}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Tortor.
+                </Text>
+              </View>
             </View>
+          </ScrollView>
+          <View style={styles.paginationWrapper}>
+            {Array.from(Array(5).keys()).map((key, index) => (
+              <View
+                style={[
+                  styles.paginationDots,
+                  { opacity: pageIndex === index ? 1 : 0.4 }
+                ]}
+                key={index}
+              />
+            ))}
           </View>
-        </ScrollView>
-        <View style={styles.paginationWrapper}>
-          {Array.from(Array(5).keys()).map((key, index) => (
-            <View
-              style={[
-                styles.paginationDots,
-                { opacity: pageIndex === index ? 1 : 0.4 }
-              ]}
-              key={index}
-            />
-          ))}
-        </View>
+        </ImageBackground>
       </SafeAreaView>
     </>
   );
@@ -135,25 +144,44 @@ const styles = StyleSheet.create({
     position: 'absolute',
     alignContent: 'center'
   },
+  image: {
+    flex: 1,
+    justifyContent: 'center'
+  },
   imageStyle: {
+    left: 150,
+    top: 50,
     height: PixelRatio.getPixelSizeForLayoutSize(35),
-    width: '100%'
+    width: '24%'
   },
   wrapper: {
     justifyContent: 'center',
     alignItems: 'center',
-    marginVertical: 0
+    marginVertical: 140,
   },
   header: {
+    width: 300,
+    height: 120,
+    left: 0,
+    top: 10,
     fontFamily: theme.fonts.primary_500,
-    fontSize: 30,
+    fontSize: 40,
     fontWeight: 'bold',
-    marginBottom: 20,
+    marginBottom: 78,
+    textAlign: 'center',
+    lineHeight: 40,
     color: '#4B5066'
   },
   paragraph: {
+    width: 254,
+    height:102,
+    left: 0,
+    top: 0,
     fontFamily: theme.fonts.primary_400,
-    fontSize: 17,
+    fontSize: 15,
+    fontWeight: '400',
+    textAlign: 'center',
+    lineHeight: 25,
     color: '#6874E8'
   },
   paginationWrapper: {
